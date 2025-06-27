fd1ef0183acf23ea2eb2a7c007ca547ccd1512dd
perkelen <omchyn@gmail.com>
2025-06-27 20:42:21 +0300

    Update UserCountryMap.php

--- UserCountryMap.php
+++ UserCountryMap.php
@@ -1,19 +1,8 @@
 <?php
 /**
- * Piwik - Open source web analytics
- *
- * @link http://piwik.org
- * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
- * @version $Id$
- *
- * @category Piwik_Plugins
- * @package Piwik_UserCountryMap
+ * Open source web analytics
  */
 
-/**
- *
- * @package Piwik_UserCountryMap
- */
 class Piwik_UserCountryMap extends Piwik_Plugin
 {
     public function getInformation()
