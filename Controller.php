6e3729386560dde29a28e6a3cdc6fec6490da968
perkelen <omchyn@gmail.com>
2025-06-27 20:41:22 +0300

    Update Controller.php

--- Controller.php
+++ Controller.php
@@ -1,19 +1,4 @@
-<?php
-/**
- * Piwik - Open source web analytics
- *
- * @link http://piwik.org
- * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
- * @version $Id$
- *
- * @category Piwik_Plugins
- * @package Piwik_UserCountryMap
- */
 
-/**
- *
- * @package Piwik_UserCountryMap
- */
 class Piwik_UserCountryMap_Controller extends Piwik_Controller
 {
 
